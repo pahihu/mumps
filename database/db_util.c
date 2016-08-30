e12f1e6aa218b688137c23b1fbf20a2fbc4f3267
pahihu <pahia@t-online.hu>
2016-08-30 10:16:06 +0200

    restored stable Locate()

--- database/db_util.c
+++ database/db_util.c
@@ -168,13 +168,10 @@ short Insert(u_char *key, cstring *data)                // insert a node
 // Note:     Must hold a write lock before calling this function
 //
 
-extern u_int LastBlock;                                 // see db_locate.c
-
 void Queit()						// que a gbd for write
 { int i;						// a handy int
   gbd *ptr;						// a handy ptr
 
-  LastBlock = 0;                                        // zot Locate() cache
   ptr = blk[level];					// point at the block
   ptr->blkver_low++;
   systab->vol[volnum-1]->stats.logwt++;			// incr logical
