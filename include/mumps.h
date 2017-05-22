c91222d15712192278eab9b1a22ce3a18e8b8ef7
Andras Pahi <pahia@t-online.hu>
2017-05-22 14:01:35 +0200

    changed major DB version

--- include/mumps.h
+++ include/mumps.h
@@ -150,8 +150,8 @@
 #define AVROUSIZ        3072                    // average compiled routine size
 #define MAXROUSIZ       32767                   // max compiled rou size
 #define MAXROULIN       32767                   // max rou lines
-#define COMP_VER        (7+(MAX_NAME_BYTES-8)*256)  // compiler version
-#define DB_VER          (1+(MAX_NAME_BYTES-8)*256)  // database version
+#define COMP_VER        (8+(MAX_NAME_BYTES-8)*256)  // compiler version
+#define DB_VER          (2+(MAX_NAME_BYTES-8)*256)  // database version
 
 // Global flags (from Global Directory) follow
 #define GL_JOURNAL      1                       // Journal global flag
