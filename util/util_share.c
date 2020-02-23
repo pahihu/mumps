31c967f9d6e17d434efcb763a8a617da712b1a28
pahihu <pahihu@gmail.com>
2020-02-23 18:14:11 +0100

    DB callback only when writing

--- util/util_share.c
+++ util/util_share.c
@@ -188,7 +188,9 @@ short SemOpEx(int sem_num, int numb,
 
   for (i = 0; i < 5; i++)                       // try this many times
   { if (0 == i)                                 // first iteration ?
-    { if ((SEM_GLOBAL == sem_num) && (1 < numb))//   GLOBAL unlock ?
+    { if ((SEM_GLOBAL == sem_num) &&            // GLOBAL lock ?
+          writing &&                            //   writing ?
+          (0 < numb))                           //   unlock ?
         DB_WillUnlock();                        //     call back
     }
     s = (numb < 0) ? SemLock(sem_num, numb) : SemUnlock(sem_num, numb);
